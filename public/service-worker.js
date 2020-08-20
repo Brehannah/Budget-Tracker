ceb616979488e1961eb649dde0827c710b22839b
Brehannah <57406410+Brehannah@users.noreply.github.com>
2020-08-19 21:51:11 -0400

    api.js added

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,7 +6,7 @@ const DATA_CACHE_NAME = "data-cache-v1";
 self.addEventListener("install", function (evt) {
     evt.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
-            console.log("Your files were pre-cached successfully!");
+            console.log(" Pre-cached successful");
             return cache.addAll(FILES_TO_CACHE);
         })
     );
